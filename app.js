761e8baf24014102043b45cfe7f5da5d374e09fb
Rakesh Shrestha <56125560+UnknownBeginner3222@users.noreply.github.com>
2020-10-12 18:36:27 +0545

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 
-var http = require('http');
+var http = require('http'); // callinf http library
 
 
-var server = http.createServer(function (req, res){
-    res.end('<h1>Server Worked!!!!</h1>');
+var server = http.createServer(function (req, res){ // Creating server
+    res.end('<h1>Server Worked!!!!</h1>'); //Setting content Headers
 });
 
-server.listen(3000, 'localhost',function(){
+server.listen(3000, 'localhost',function(){  //assigning 3000 as server listening ports
     console.log('Server started on port : 3000');
 });
